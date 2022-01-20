062a27e5a36f4df9c7fa521203253179a76c3dbd
henrykkebaby <59703378+henrykkebaby@users.noreply.github.com>
2022-01-20 13:14:31 +0100

    fixing up...

--- src/views/kebabStorageView.jsx
+++ src/views/kebabStorageView.jsx
@@ -19,9 +19,14 @@ function KebabStorageView(props) {
                 {props.list.map((item) => {
                     return (
                         item.toString().includes(".") ?
-                            <div onClick={() => props.setSelectedFile(item.toString())} key={item} style={{ height: "50px", backgroundColor: "rgb(250, 218, 77)", marginTop: "15px", textAlign: "left", display: "flex", flexDirection: "row", cursor: "pointer" }}>
-                                <img style={{ marginLeft: "10px", marginTop: "10px", marginRight: "15px", userSelect: "none" }} src={fileIcon} height={"35px"} onClick={() => props.getFile(item.toString())} />
-                                <h2 style={{ whiteSpace: "nowrap", marginTop: "10px", userSelect: "none" }}>{item.toString()}</h2>
+                            <div style={{ display: "flex", flexDirection: "row", cursor: "pointer" }}>
+                                <div onClick={() => props.setSelectedFile(item.toString())} key={item} style={{ flex: "1", height: "50px", backgroundColor: "rgb(250, 218, 77)", marginTop: "15px", textAlign: "left", cursor: "pointer", display: "flex", flexDirection: "row" }}>
+                                    <img style={{ marginLeft: "10px", marginTop: "10px", marginRight: "15px", userSelect: "none" }} src={fileIcon} height={"35px"} onClick={() => props.getFile(item.toString())} />
+                                    <h2 style={{ whiteSpace: "nowrap", marginTop: "10px", userSelect: "none" }}>{item.toString()}</h2>
+                                </div>
+                                <div style={{ width: "30px", height: "50px", backgroundColor: "rgb(250, 218, 77)", cursor: "pointer", marginTop: "15px" }}>
+                                    <img onClick={() => { props.remFile(props.selectedFile); props.setSelectedFile(null) }} height="20px" src={trashIcon} style={{ marginRight: "0px", marginLeft: "auto", marginTop: "16px", userSelect: "none" }} />
+                                </div>
                             </div>
                             :
                             <div style={{ display: "flex", flexDirection: "row" }}>
