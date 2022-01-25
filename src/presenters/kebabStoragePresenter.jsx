9c17f62d6552a30dff03d0b29a2beb4c79a7ca33
henrykkebaby <59703378+henrykkebaby@users.noreply.github.com>
2022-01-25 14:00:40 +0100

    folder fully working

--- src/presenters/kebabStoragePresenter.jsx
+++ src/presenters/kebabStoragePresenter.jsx
@@ -220,8 +220,11 @@ function KebabStoragePresenter(props) {
   return (
     <div style={{ display: "flex", flexDirection: "row", position: "absolute", bottom: "0px", top: "1.5cm", left: "0px", right: "0px" }}>
 
-      {isCreatingFolder && <input id="folderInputMobile" type="text" autoComplete="off" style={{ position: "fixed", height: "60px", width: "100%", left: "50%", bottom: "1.5cm", transform: "translate(-50%, 0)", fontSize: "0.7cm" }} placeholder="Create a folder here..." onKeyPress={event => { if (event.key === 'Enter') { createFolder(); setIsCreatingFolder(false); } }} />}
-
+      {isCreatingFolder &&
+        <div style={{ position: "fixed", width: "100%", left: "50%", ight: "60px", bottom: "1.5cm", transform: "translate(-50%, 0)", display: "flex" }}>
+          <input id="folderInputMobile" type="text" autoComplete="off" style={{ height: "60px", flex: "7", fontSize: "0.5cm" }} placeholder="Create a folder here..." onKeyPress={event => { if (event.key === 'Enter') { createFolder(); setIsCreatingFolder(false); } }} />
+          <button style={{ flex: 1 }} onClick={() => setIsCreatingFolder(false)}>X</button>
+        </div>}
       {isMobile &&
 
         <KebabStorageMenuMobileView
