94f4facaca2bb18a85448f9849d9fd153b00b1f4
henrykkebaby <59703378+henrykkebaby@users.noreply.github.com>
2022-01-25 13:17:17 +0100

    ui fix

--- src/views/kebabStorageViews/kebabStorageMenuMobileView.jsx
+++ src/views/kebabStorageViews/kebabStorageMenuMobileView.jsx
@@ -7,14 +7,14 @@ import addFolderIcon from "../../files/addFolderIcon.png"
 
 function KebabStorageMenuView(props) {
     return (
-        <div style={{ position: "fixed", bottom: "0cm", left: "0cm", right: "0cm", height: "2cm", backgroundColor: "rgb(245, 245, 245)", borderTop: "2px solid black" }}>
+        <div style={{ position: "fixed", bottom: "0cm", left: "0cm", right: "0cm", height: "1.5cm", backgroundColor: "rgb(245, 245, 245)", borderTop: "2px solid black" }}>
 
-            {(props.isLoading || props.isListLoading) ? <img src={spinnerIcon} style={{ height: "2cm" }} /> :
+            {(props.isLoading || props.isListLoading) ? <img src={spinnerIcon} style={{ height: "1.5cm", marginLeft: "50%", transform: "translate(-50%, 0)" }} /> :
                 <div style={{ display: "flex", justifyContent: "space-evenly" }}>
                     <input id="fileUploader" type="file" multiple="multiple" onChange={(e) => props.fileUpload(e.target.files)} style={{ display: "none" }} />
-                    <label htmlFor="fileUploader" > <img src={uploadIcon} style={{ height: "2cm" }} /></label>
-                    <img onClick={() => props.getDir(props.path)} src={refreshIcon} style={{ height: "2cm" }} />
-                    <img src={addFolderIcon} style={{ height: "2cm" }} />
+                    <label htmlFor="fileUploader" > <img src={uploadIcon} style={{ height: "1.5cm" }} /></label>
+                    <img onClick={() => props.getDir(props.path)} src={refreshIcon} style={{ height: "1.5cm" }} />
+                    <img src={addFolderIcon} style={{ height: "1.5cm" }} />
                 </div>
             }
         </div>
